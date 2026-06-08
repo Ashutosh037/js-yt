16ca905e5e18a47566392fefabe969d5c30b5741
Ashutosh Singh <thakur912565@gmail.com>
2026-06-08 05:11:23 +0000

    comparison

---
+++ 01-basics/04_comparison.js
@@ -0,0 +1,9 @@
+// console.log("2" > 1);   //true
+// console.log("02" > 1);  //true
+
+console.log(null > 0);  //false
+console.log(null == 0);  //false
+console.log(null >= 0);   //true
+//comparison converts null to a number treating it as zero
+
+// === check datatype also, it checks strictly
