1d738c772ec9a65c56965353e20f7bc8af2dac9c
Ashutosh Singh <thakur912565@gmail.com>
2026-06-08 14:02:47 +0000

    comparison

--- 01-basics/04_comparison.js
+++ 01-basics/04_comparison.js
@@ -7,3 +7,8 @@ console.log(null >= 0);   //true
 //comparison converts null to a number treating it as zero
 
 // === check datatype also, it checks strictly
+
+
+//**************** STACK(Primitive) and HEAP(Non-Primitive) Memory ***********************************
+// we always get copy in stack while in heap we get same reference
+
