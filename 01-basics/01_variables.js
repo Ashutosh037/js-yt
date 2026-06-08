7281e278029b9f1280bcaea34dc3fe428e96dd37
Ashutosh Singh <thakur912565@gmail.com>
2026-06-08 03:46:39 +0000

    dataTypes

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,14 @@
+let accoundId = "ashu@gmail.com"
+const accountName = "ashu"
+var accountPassword = "12345"
+accountCity = "Mau"
+
+accountId="ashusingh@gmail.com"
+//accountName = "ashusingh"
+accountPassword = "232323"
+
+/*
+Prefer not to use because of issue in block scope and functional scope
+*/
+console.table([accountId, accountPassword, accountCity])
+
