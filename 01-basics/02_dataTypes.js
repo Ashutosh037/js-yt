105f64ac5e33ee86fc925bd2d69688c4dcba0806
Ashutosh Singh <thakur912565@gmail.com>
2026-06-08 03:50:22 +0000

    dataTypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict";    // treat all js code as newer version
+
+// alert(2+3)  //it gives error because we are using node.js not browser
+
+// num
+//string
+//boolean -> true or false
+//bigint
+// null -> standalone value
+// undefined -> value is not assigned yet
+// symbol -> unique
+
+console.log(typeof undefined);
+console.log(typeof null);
