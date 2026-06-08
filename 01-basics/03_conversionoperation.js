7a2b5c38a36fdb87317f96a6a0b5131d62f18d81
Ashutosh Singh <thakur912565@gmail.com>
2026-06-08 04:11:40 +0000

    conversionofdatatypes

---
+++ 01-basics/03_conversionoperation.js
@@ -0,0 +1,17 @@
+let score = "24"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let scoreInNumber = Number(score)
+//console.log(typeof scoreInNumber);
+
+// "" => false
+// "ashu" => true
+// 1 => true
+// 0 => false
+
+let someNumber = 25
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
