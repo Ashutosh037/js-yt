6cec7ba2f070f48f82eb65c850ed271046a15b45
Ashutosh Singh <thakur912565@gmail.com>
2026-06-09 04:22:54 +0000

    nums_and_maths

---
+++ 01-basics/06_nums_and_maths.js
@@ -0,0 +1,20 @@
+const balance = new Number(500)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 24.6924
+// console.log(otherNumber.toPrecision(1));
+
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// ***************MATHS************
+
+// console.log(Math.round(3.4));
+// console.log(Math.round(3.5));
+// console.log(Math.round(3.6));
+
+console.log(Math.random());   // in this the value always comes between 0 and 1
