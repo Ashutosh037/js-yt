ebd00b5b86e8f877edd34bc0be412e0205d47684
Ashutosh Singh <thakur912565@gmail.com>
2026-06-10 04:36:13 +0000

    array

---
+++ 02-basics/01_array.js
@@ -0,0 +1,20 @@
+const myarr = [1,2,3,4,5]
+// console.log([0])
+
+
+// myarr.push(6)  // it adds the element in the last
+// console.log(myarr)
+
+// myarr.pop()  //it removes the last element 
+
+// myarr.unshift(0) //it adds the element in the starting
+
+//console.log(myarr.includes(9))  //it checks the element is included or not and return the boolean value
+//console.log(myarr)
+
+const newarr = myarr.join()  //it joins the element and converted it into a string
+console.log(newarr)
+console.log(typeof newarr)
+
+//slice
+//splice
