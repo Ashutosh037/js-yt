43e7abbc9c0ba88a174ea2e424ee5ee64def5201
Ashutosh Singh <thakur912565@gmail.com>
2026-06-12 04:43:53 +0000

    array

---
+++ 02-basics/02_array.js
@@ -0,0 +1,9 @@
+const marvel_h = ["thor"]
+const dc_h = ["aqua","super"]
+
+//spread to merge two arrays into one    you can also use concat
+
+const myna = [...marvel_h, ...dc_h]
+console.log(myna)
+
+//flat can be used to convert every array that is inside an array into one array
