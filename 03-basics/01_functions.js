f483f7292e364b0a50a44ed0a12008c6d518354b
Ashutosh Singh <thakur912565@gmail.com>
2026-06-13 09:10:15 +0000

    functions

---
+++ 03-basics/01_functions.js
@@ -0,0 +1,20 @@
+// function sayMyName() {
+//     console.log("A")
+//     console.log("s")
+//     console.log("h")
+//     console.log("u")
+// }
+
+// sayMyName()   //sayMyName is reference and () is for calling
+
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+// }
+// add(2, 3)
+
+// function logInUserMsg(username) {
+//     return `${username} just logged in`
+// }
+// console.log(logInUserMsg("Ashutosh"));
+
+
