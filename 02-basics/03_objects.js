e9c6658ecc743f837b33c218ab9de46f98f69e07
Ashutosh Singh <thakur912565@gmail.com>
2026-06-13 04:26:50 +0000

    json

--- 02-basics/03_objects.js
+++ 02-basics/03_objects.js
@@ -3,13 +3,27 @@
 //object.create
 
 
-const JsUser = {
-    "name": "Ashutosh",  //this value can't be accessed through dot so we use string like
-    age: 19,
-    email: "ashu@gmail.com",
-    isLoggedIn: false,
-    lastLoginDays: ["Monday", "Thursday"]
+// const JsUser = {
+//     "name": "Ashutosh",  //this value can't be accessed through dot so we use string like
+//     age: 19,
+//     email: "ashu@gmail.com",
+//     isLoggedIn: false,
+//     lastLoginDays: ["Monday", "Thursday"]
+// }
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])  //here we have given like string
+
+
+// ************************OBJECT DE-STRUCTURE**********************
+const course = {
+    coursename: "js",
+    price : "999",
+    courseInstructor : "Hitesh",
 }
 
-console.log(JsUser.email)
-console.log(JsUser["email"])  //here we have given like string
+//course.courseImstructor
+const {courseInstructor: Instructor} = course
+console.log(Instructor);
+
+//json -- javascript object notation
